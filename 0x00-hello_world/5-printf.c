b10c51131a210a1386198aa0de696fd755586a2e
amorizitha <amorezitha@gmail.com>
2023-05-18 05:52:15 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,10 @@
 /**
  * main - Entry point
  *
-<<<<<<< HEAD
  * Return: Always 0 (success)
-=======
- * Return Always 0 (Success)
->>>>>>> f45076245faf83d32e69688f8ed6c066ed701b1a
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0)
+	return (0);
 }
