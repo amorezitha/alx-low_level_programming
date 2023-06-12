a1f5e2007f7cf1dbd3ae779984aeabd4a26914e4
amorizitha <amorezitha@gmail.com>
2023-06-12 02:40:52 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,24 +10,34 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *concat;
+	char *conct;
 	int i, ci;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
+
+	if (conct == NULL)
+		return (NULL);
 	i = ci = 0;
 	while (s1[i] != '\0')
 	{
-		concat[i] = s1[i];
+		conct[i] = s1[i];
 		i++;
 	}
-	while (s2[ci] != '\0')
+	while (s2[i] != '\0')
 	{
-		concat[i] = s2[ci];
+		conct[i] = s2[ci];
 		i++, ci++;
 	}
-	concat[i] = '\0';
-	return (concat);
+	conct[i] = '\0';
+	return (conct);
 }
