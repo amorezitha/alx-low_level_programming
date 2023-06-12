de32381f1352159dfeeccc90f7ff7792e5403f8c
amorizitha <amorezitha@gmail.com>
2023-06-12 03:12:00 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,11 +32,11 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	 for (x = 0; x < height; x++)
-	 {
-		 for (y = 0; y < width; y++)
-			 mee[x][y] = 0;
-	 }
-	 return (mee);
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			mee[x][y] = 0;
+	}
+	return (mee);
 }
 
