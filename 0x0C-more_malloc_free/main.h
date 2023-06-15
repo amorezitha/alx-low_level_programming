f694d84b99dec73dbc97da038ae7018b9b32b081
amorizitha <amorezitha@gmail.com>
2023-06-15 00:37:58 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,8 +7,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int _putchar(char c);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void _putchar(char *str);
-int _atou(const char *s);
-void print _int(unsigned long int n);
+void _puts(char *str);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
 
 #endif
