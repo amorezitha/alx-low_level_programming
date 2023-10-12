8e115f1f61f480b89a054fd106487fea3db32cbb
amorezitha <amorezitha@gmail.com>
2023-10-12 10:55:09 -0700

    update

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - sum of elements in list
- * @head: list
- * Return: int
+ * sum_dlistint - returns the sum of all ints 'n' of a dlistint_t linked list
+ * @head: pointer to first member of dlistint_t list
+ * Return: sum of all ints 'n', or 0 of list is empty
  */
 
 sum_dlistint(dlistint_t *head)
