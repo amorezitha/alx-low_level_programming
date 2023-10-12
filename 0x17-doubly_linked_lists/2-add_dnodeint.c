9b6ddbd79cab78d28f3478fe8be77a74c8c02ead
amorezitha <amorezitha@gmail.com>
2023-10-12 06:47:05 -0700

    update

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->next = *head;
 	new->n = n;
 	if (*head != NULL)
-		(*heas)->prev = new;
+		(*head)->prev = new;
 	*head = new;
 	return (new);
 }
