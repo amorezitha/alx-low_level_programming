ec5693d7b4e68fbd9db78cfaebc1db50b9d41ace
amorezitha <amorezitha@gmail.com>
2023-11-02 22:07:28 -0700

    update

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,9 +1,11 @@
+#include "hash_tables.h"
+
 /**
- *  * hash_djb2 - implementation of the djb2 algorithm
- *   * @str: string used to generate hash value
- *    *
- *     * Return: hash value
- *      */
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
@@ -13,6 +15,6 @@ unsigned long int hash_djb2(const unsigned char *str)
 	while ((c = *str++))
 	{
 			hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-		}
+	}
 	return (hash);
 }
